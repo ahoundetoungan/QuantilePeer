370cf9cc044e88fe775eb366e60cf9d7d623bf16
ahoundetoungan <ariel92and@gmail.com>
2025-03-21 03:51:55 +0100

    version 1.0.0

--- R/QuantilePeer.R
+++ R/QuantilePeer.R
@@ -1,8 +1,10 @@
 #' @title The QuantilePeer package
-#' @description The \pkg{QuantilePeer} package simulates and estimates quantile peer effect models that are introduced by Houndetoungan (2025). 
+#' @description The \pkg{QuantilePeer} package simulates and estimates peer effect models including the quantile-based specification (Houndetoungan, 2025), and the models with Constant Elasticity of Substitution (CES)-based social norm (Boucher et al., 2024 \doi{10.3982/ECTA21048}).
 #'
 #' @importFrom Rcpp sourceCpp
 #' @importFrom Matrix Matrix
+#' @references Boucher, V., Rendall, M., Ushchev, P., & Zenou, Y. (2024). Toward a general theory of peer effects. Econometrica, 92(2), 543-565, \doi{10.3982/ECTA21048}.
+#' @references Houndetoungan, A. (2025). Quantile peer effect models.
 #' @references Hyndman, R. J., & Fan, Y. (1996). Sample quantiles in statistical packages. The American Statistician, 50(4), 361-365, \doi{10.1080/00031305.1996.10473566}.
 #' @useDynLib QuantilePeer, .registration = TRUE
 "_PACKAGE"
